fe5a046e8f1cd515e38065b984eb43b040f230c7
Mehran Rahbardar <mrahbardar@users.noreply.github.com>
2017-06-20 13:51:53 +0430

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    sso:'__http://SERVICE.ENDPOINT/oauth__',
+    sso_service:'__http://SERVICE.ENDPOINT/_',
+    client_id:'__CLIENT_ID__"',
+    client_secret:'__CLIENT_SECRET__',
+    home:'http://WHERE_THIS_CODE_DEPLOYED/',
+
+};
